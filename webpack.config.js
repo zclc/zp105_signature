808aba8248d550fc8eb6b6853128db9ef71e7177
众触应用平台 <87555332+zcappp@users.noreply.github.com>
2024-02-13 08:35:32 +0800

    1

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: process.env.mode || "development",
     devtool: false,
     entry: {
-        "zp105": "./plugins/zp105_手写签名.js",
+        "zp105": "./src/zp105_手写签名.js",
     },
     output: {
         path: path.join(__dirname, "build/"),
